110355863e4605636c8e993580bcc87925f8d5f1
arnold-O <arnoldoghiator@gmail.com>
2024-10-22 22:33:49 +0100

    formating price

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import { formatprice } from '../utils';
 
 const ProductGrid = () => {
     const {products} = useLoaderData();
@@ -9,6 +10,7 @@ const ProductGrid = () => {
         <div className='p-12 grid gap-4 lg:grid-cols-3 md:grid-cols-2'>
             {products.data.map((item)=>{
                 const {title,price,image} = item.attributes;
+            const priceFormat =  formatprice(price)
             return <Link className='card shadow-xl hover:shadow-2xl transition duration-500 w-full' key={item.id} to={ `/products/${item.id}`}  >
 
                 <section className=' px-4 pt-4'>
@@ -17,7 +19,7 @@ const ProductGrid = () => {
 
                     <div className="card-body items-center text-center">
                         <h2 className='card-title capitalize tracking-wide'>{title}</h2>
-                        <span className='title-secondary'>{price}</span>
+                        <span className='title-secondary'>{priceFormat}</span>
                     </div>
 
                 </section>
--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,11 @@ const baseUrl = 'https://strapi-store-server.onrender.com/api';
 export const customFetch = axios.create({
     baseURL:baseUrl,
 })
+
+export const formatprice = (price)=>{
+    const NairaAmount = new Intl.NumberFormat('en-NGN', {
+        currency: 'NGN',
+        style: 'currency',
+    }).format((price/100).toFixed(2));
+    return NairaAmount
+}
