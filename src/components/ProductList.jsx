a2c0f47d37f1bcf7efce60f8c1873623620f3b2b
arnold-O <arnoldoghiator@gmail.com>
2024-10-31 00:11:49 +0100

    productlist and productgrid css modify

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { formatprice } from '../utils';
 
 const ProductList = () => {
     const {products} = useLoaderData();
    
    
     return (
-        <div className='p-12 grid gap-4 lg:grid-cols-3 md:grid-cols-2'>
+        <div className='mt-12 grid gay-y-8'>
             {products.data.map((item)=>{
-                const {title,price,image} = item.attributes;
+                const {title,price,image, company} = item.attributes;
             const priceFormat =  formatprice(price)
-            return <Link className='card shadow-xl hover:shadow-2xl transition duration-500 w-full' key={item.id} to={ `/products/${item.id}`}  >
+            return <Link className=' p-8 rounded-lg flex flex-col sm:flex-row gay-y-4 flex-wrap bg-base-300 shadow-xl hover:shadow-2xl duration-300 group' key={item.id} to={ `/products/${item.id}`}  >
 
-                <section className=' px-4 pt-4'>
+                {/* <section className=' px-4 pt-4'> */}
 
-                    <img src={image} alt={title} className='object-cover rounded-xl h-64 md:h-48 w-full'  />
+                    <img src={image} alt={title} className='h-24 w-24  object-cover rounded-lg  sm:h-32 sm:w-32 group-hover:scale-105 transiion duration-300'  />
 
-                    <div className="card-body items-center text-center">
-                        <h2 className='card-title capitalize tracking-wide'>{title}</h2>
-                        <span className='title-secondary'>{priceFormat}</span>
+                    <div className="ml-0 sm:ml-16">
+                        <h3 className='capitalize font-medium text-lg '>{title}</h3>
+                        <h4 className='capitalize text-md text-neutral-content'>{company}</h4>
                     </div>
 
-                </section>
+                {/* </section> */}
             
             </Link>
             })}
