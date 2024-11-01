467873e2f8b7095a80ad8a49aacd0848d14ced96
arnold-O <arnoldoghiator@gmail.com>
2024-11-01 15:48:12 +0100

    select, category,order input added

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useLoaderData } from 'react-router-dom';
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 const Filters = () => {
+    const {meta} = useLoaderData()
     return (
         <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             
-            <FormInput type='search' label='search products' name='search' size='imput-sm'/>
+            <FormInput type='search' label='search products' name='search' size='input-sm'/>
+            <FormSelect name='company' label='select company' size='select-sm' list={meta.companies}/>
+            <FormSelect name='category' label='select category' size='select-sm' list={meta.categories}/>
+            <FormSelect name='order' label='sort by' size='select-sm' list={['a-z', 'z-a', 'high', 'low']}/>
             <button className='btn btn-primary btn-sm'>search</button>
 
             <Link to='/products' className='btn btn-accent btn-sm'>reset</Link>
