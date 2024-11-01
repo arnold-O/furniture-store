3c36922fa160bac2a3542d8eff7a0a2d2b4d6161
arnold-O <arnoldoghiator@gmail.com>
2024-11-01 13:18:33 +0100

    added search component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Form, Link } from 'react-router-dom';
+import FormInput from './FormInput';
 
 const Filters = () => {
     return (
-        <div>
+        <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             
-        </div>
+            <FormInput type='search' label='search products' name='search' size='imput-sm'/>
+            <button className='btn btn-primary btn-sm'>search</button>
+
+            <Link to='/products' className='btn btn-accent btn-sm'>reset</Link>
+
+            
+        </Form>
     );
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import SubmitBtn from './SubmitBtn';
 import { Link } from 'react-router-dom';
 
-const FormInput = ({label, name, type, defaultValue}) => {
+const FormInput = ({label, name, type, defaultValue, size}) => {
     return (
         <div className="form-control">
             <label className="label">
                
-                    <span className="label-text">{label}</span>
+                    <span className="label-text capitalize">{label}</span>
                   
              </label>
-                <input type={type} name={name} defaultValue={defaultValue} className="input input-bordered" />
+                <input type={type} name={name} defaultValue={defaultValue} className={`input input-bordered ${size}`}/>
                
-           
-          
         </div>
     );
 }
