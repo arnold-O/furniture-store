f3dd4e97af27bc65df9821a9fc1ff076ac1940a8
arnold-O <arnoldoghiator@gmail.com>
2024-11-03 22:12:31 +0100

    added store redux functionaity

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState = {
+    cartItem:[],
+    NumItemsInCart: 0,
+    cartTotal:0,
+    shipping:1000,
+    tax:0,
+    orderTotal:0
+}
+
+const cartSlice = createSlice({
+    name:'cart',
+    initialState,
+
+    reducers:{
+        addItem:(state, action)=>{
+
+        }
+    },
+    clearCart:(state)=>{
+
+    },
+    removeItem:(state, action)=>{
+        
+    },
+    editItem:(state, action)=>{
+
+    }
+})
+
+
+export const {addItem, clearCart, removeItem, editItem} = cartSlice.actions;
+export default cartSlice.reducer
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { Provider } from 'react-redux';
+import { store } from './store.js';
+
+
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <Provider store={store}>
+  <ToastContainer position='top-center' autoClose={2000}/>
     <App />
-  // </StrictMode>,
+  // </Provider>,
 )
