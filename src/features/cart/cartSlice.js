3cf8dee7cca290aa441338c857a86b187b3ea73e
arnold-O <arnoldoghiator@gmail.com>
2024-11-06 23:48:02 +0100

    bug of caseReducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -9,13 +9,18 @@ const initialState = {
     shipping:1000,
     tax:0,
     orderTotal:0
+};
+
+const getCartFromLocalStorage = ()=>{
+    return JSON.parse(localStorage.getItem('cart'))  || initialState;
 }
 
 const cartSlice = createSlice({
     name:'cart',
-    initialState,
+   initialState:getCartFromLocalStorage(),
 
     reducers:{
+        // Add Item
         addItem:(state, action)=>{
          const {product} = action.payload;
          const item = state.cartItems.find((item)=>item.cartID === product.cartID);
@@ -27,21 +32,30 @@ const cartSlice = createSlice({
          }
          state.NumItemsInCart += product.amount;
          state.cartTotal += product.amount * product.price;
-         state.tax = 0.1 * state.cartTotal
-         state.orderTotal = state.shipping + state.tax + state.cartTotal;
 
-         localStorage.setItem('cart', JSON.stringify(state))
+         cartSlice.caseReducers.calculateTotals(state)
+
          toast.success('Item ADDED TO CART')
 
         }
     },
+    // Clear Items
     clearCart:(state)=>{
-
     },
+
+    // remove Items
     removeItem:(state, action)=>{
         
     },
+    // edit item
     editItem:(state, action)=>{
+
+    },
+
+    calculateTotals: (state)=>{
+         state.tax = 0.1 * state.cartTotal
+         state.orderTotal = state.shipping + state.tax + state.cartTotal;
+         localStorage.setItem('cart', JSON.stringify(state))
 
     }
 })
